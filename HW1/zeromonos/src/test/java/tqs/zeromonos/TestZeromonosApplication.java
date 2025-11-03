a0d348a9cafd33bfbc863f48abe1a787e1876457
Gonçalo Simões <151958998+goncaloosimoes@users.noreply.github.com>
2025-11-03 13:38:34 +0000

    Remover pasta do zeromonos para adicionar submódulo

--- HW1/zeromonos/src/test/java/tqs/zeromonos/TestZeromonosApplication.java
+++
@@ -1,9 +0,0 @@
-package tqs.zeromonos;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestZeromonosApplication {
-	public static void main(String[] args) {
-		SpringApplication.from(ZeromonosApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-}
