457a1416aa4b6f5864d566ab7aa2058cf6780fa2
Gonçalo Simões <151958998+goncaloosimoes@users.noreply.github.com>
2025-11-03 14:08:31 +0000

    Remove labs/HW1 to prepare submodule

--- HW1/zeromonos/src/test/java/tqs/zeromonos/TestcontainersConfiguration.java
+++
@@ -1,7 +0,0 @@
-package tqs.zeromonos;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-public class TestcontainersConfiguration {
-}
